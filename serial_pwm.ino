f0509c6ed1f9d4afa6372c15e026c8ae347b6b22
jerkey <jerkey@halo.nu>
2014-02-10 06:06:09 -0800

    perfectly controls DEGREES FARENHEIT DEGREES CELSIUS giant analog meter

--- serial_pwm.ino
+++ serial_pwm.ino
@@ -1,5 +1,5 @@
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
   pinMode(9, OUTPUT); 
 }
 
@@ -7,13 +7,18 @@ void loop() {
   while (Serial.available() > 0) {
 
     // look for the next valid integer in the incoming serial stream:
-    int pwmValue = Serial.parseInt(); 
+    // int pwmValue = Serial.parseInt(); 
+    float celsius = Serial.parseFloat();
     // look for the newline. That's the end of your
     if (Serial.read() == '\n') {
-      // constrain the values to 0 - 255
-      pwmValue = constrain(pwmValue, 0, 255);
-      Serial.println(pwmValue);
-      analogWrite(9,pwmValue);
+  // PWM of 24 = 0 celsius
+  //  114 = 20 C
+  //  210 = 40 C
+  //  from 20 to 40 degrees = 96 PWM so 4.8 PWM per degree
+  //  PWM = (celsius * 4.8) + 18
+      int pwm = constrain((int)(celsius * 4.8) + 18, 0, 255);
+      analogWrite(9,pwm);
+      Serial.println(pwm);
     }
   }
 }
