030db81e75a24d2e8d336009d908ba5bf1d75dfe
jake waters <jerkey@g-cipher.net>
2015-09-11 21:07:08 -0700

    make a sinewave synthesizer with aep

--- serial_pwm.ino
+++ serial_pwm.ino
@@ -1,24 +1,95 @@
+byte sineTable[180];
+
 void setup() {
   Serial.begin(57600);
   pinMode(9, OUTPUT); 
+  setPwmFrequency(9, 1);
+  Serial.println("serial_pwm");
+  for (int i = 0; i < 180; i++) {
+    sineTable[i] = (byte)(255.0 * sin((3.14159/180.0)*i));
+  }
 }
+
+int lastPwm, pwm, cycle = 0;
+int scale = 127;
 
 void loop() {
   while (Serial.available() > 0) {
 
-    // look for the next valid integer in the incoming serial stream:
-    // int pwmValue = Serial.parseInt(); 
     float celsius = Serial.parseFloat();
-    // look for the newline. That's the end of your
     if (Serial.read() == '\n') {
-  // PWM of 24 = 0 celsius
-  //  114 = 20 C
-  //  210 = 40 C
-  //  from 20 to 40 degrees = 96 PWM so 4.8 PWM per degree
-  //  PWM = (celsius * 4.8) + 18
-      int pwm = constrain((int)(celsius * 4.8) + 18, 0, 255);
-      analogWrite(9,pwm);
-      Serial.println(pwm);
+      scale = constrain((int)(celsius), 0, 255);
+      Serial.println(scale);
     }
+  }
+  /*
+   * pwm = (int)(abs(sin((float)millis()/1000.0*2*3.14159*60.0)) * scale);
+  if (lastPwm != pwm) {
+    analogWrite(9,pwm);
+    lastPwm = pwm;
+    //Serial.println(pwm);
+  }
+  */
+  analogWrite(9,sineTable[cycle]);
+  cycle++;
+  cycle %= 180;
+}
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+    case 1: 
+      mode = 0x01; 
+      break;
+    case 8: 
+      mode = 0x02; 
+      break;
+    case 64: 
+      mode = 0x03; 
+      break;
+    case 256: 
+      mode = 0x04; 
+      break;
+    case 1024: 
+      mode = 0x05; 
+      break;
+    default: 
+      return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } 
+    else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } 
+  else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+    case 1: 
+      mode = 0x01; 
+      break;
+    case 8: 
+      mode = 0x02; 
+      break;
+    case 32: 
+      mode = 0x03; 
+      break;
+    case 64: 
+      mode = 0x04; 
+      break;
+    case 128: 
+      mode = 0x05; 
+      break;
+    case 256: 
+      mode = 0x06; 
+      break;
+    case 1024: 
+      mode = 0x7; 
+      break;
+    default: 
+      return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
   }
 }
