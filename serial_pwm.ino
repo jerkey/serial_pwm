fcd993683837be96e1c327a53c440898887c7704
jake waters <jerkey@g-cipher.net>
2015-09-12 20:38:24 -0700

    fill in coefficients & print, add setPwmFrequency() for later

--- serial_pwm.ino
+++ serial_pwm.ino
@@ -3,12 +3,12 @@
 #define BATT_I A2
 #define AC_I A4
 #define CHARGE_PWM 9
-#define VOLT_COEFF 1.0
-#define AC_I_COEFF 1.0
-#define AC_I_ZERO 178.5
-#define BATT_I_COEFF 1.0
-#define BATT_I_ZERO 512 // ADC value at zero amps
-#define OVERHEAT_SENSE 11 // gets shorted to ground when overheating
+#define VOLT_COEFF 2.366
+#define AC_I_COEFF 8.888
+#define AC_I_ZERO 181
+#define BATT_I_COEFF 8.371
+#define BATT_I_ZERO 508.7 // ADC value at zero amps
+#define OVERHEAT_SENSE 12 // gets shorted to ground when overheating
 #define RED_LEDS 5
 #define GREEN_LEDS 6
 #define BLUE_LEDS 11
@@ -45,6 +45,7 @@ void loop() {
       pwmValue = oldPwmValue;
     }
   }
+  getVolts();
   if ( millis() - displayTime > 1000 ) {
     printDisplay();
     displayTime = millis();
@@ -91,4 +92,65 @@ float averageRead(int pin) {
   for (int i = 0; i < 50; i++) analogAdder += analogRead(pin);
   analogAdder /= 50;
   return analogAdder;
+}
+
+// Note that the base frequency for pins 3, 9, 10, and 11 is 31250 Hz
+// Note that the base frequency for pins 5 and 6 is 62500 Hz
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+    case 1:
+      mode = 0x01;
+      break;
+    case 8:
+      mode = 0x02;
+      break;
+    case 64:
+      mode = 0x03;
+      break;
+    case 256:
+      mode = 0x04;
+      break;
+    case 1024:
+      mode = 0x05;
+      break;
+    default:
+      return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    }
+    else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  }
+  else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+    case 1:
+      mode = 0x01;
+      break;
+    case 8:
+      mode = 0x02;
+      break;
+    case 32:
+      mode = 0x03;
+      break;
+    case 64:
+      mode = 0x04;
+      break;
+    case 128:
+      mode = 0x05;
+      break;
+    case 256:
+      mode = 0x06;
+      break;
+    case 1024:
+      mode = 0x7;
+      break;
+    default:
+      return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
 }
